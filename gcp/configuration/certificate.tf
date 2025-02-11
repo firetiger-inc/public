db0e87535d15d947dbd16ea6432d020fc87b75a1
John Pugliesi <jpugliesi12@gmail.com>
2025-02-11 13:23:12 -0800

    reformat subdomain

--- gcp/configuration/certificate.tf
+++ gcp/configuration/certificate.tf
@@ -1,8 +1,8 @@
 locals {
-  subdomain_name = format("%s.firetigerapi.com.", var.bucket)
+  subdomain_name = format("%s.firetigerapi.com", var.bucket)
 }
 
 resource "google_dns_managed_zone" "subdomain_zone" {
   name     = var.bucket
-  dns_name = local.subdomain_name
+  dns_name = format("%s.", local.subdomain_name)
 }
