aed6c9ed182663c4f4d87fb654a23326f4918a30
John Pugliesi <jpugliesi12@gmail.com>
2025-02-11 16:21:38 -0800

    Support configurable bigquery dataset name

--- gcp/configuration/variables.tf
+++ gcp/configuration/variables.tf
@@ -6,8 +6,14 @@ variable "region" {
   type = string
 }
 
+variable "bigquery_dataset_name" {
+  type    = string
+  default = null
+}
+
 locals {
-  tables = ["logs", "metrics", "traces"]
+  bigquery_dataset_name = var.bigquery_dataset_name != null ? var.bigquery_dataset_name : replace(var.bucket, "/[^a-zA-Z0-9_]+/", "_")
+  tables                = ["logs", "metrics", "traces"]
 }
 
 data "google_project" "current" {}
