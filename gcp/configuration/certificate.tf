159235e7f49c04d110df4c5d7fa7f03158036cfa
John Pugliesi <jpugliesi12@gmail.com>
2025-02-12 18:41:18 -0800

    sanitize cloud dns zone name

--- gcp/configuration/certificate.tf
+++ gcp/configuration/certificate.tf
@@ -1,8 +1,10 @@
 locals {
+  # can only contain alphnumerics and dashes:
+  # https://cloud.google.com/dns/docs/error-messages#invalidfieldvalue
   subdomain_name = format("%s.firetigerapi.com", var.bucket)
 }
 
 resource "google_dns_managed_zone" "subdomain_zone" {
-  name     = var.bucket
+  name     = replace(var.bucket, "/[^a-zA-Z0-9-]+/", "-")
   dns_name = format("%s.", local.subdomain_name)
 }
