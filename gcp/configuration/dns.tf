d2d20aedffd0c26d339b47962af7ee12800f01dd
John Pugliesi <jpugliesi12@gmail.com>
2025-02-13 09:02:38 -0800

    Sanitize subodmain name to only alphanumerics and dashes

---
+++ gcp/configuration/dns.tf
@@ -0,0 +1,6 @@
+resource "google_dns_managed_zone" "subdomain_zone" {
+  name = local.deployment
+  # can only contain alphnumerics and dashes:
+  # https://cloud.google.com/dns/docs/error-messages#invalidfieldvalue
+  dns_name = format("%s.firetigerapi.com.", local.deployment)
+}
